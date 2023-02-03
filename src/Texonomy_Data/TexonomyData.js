dd12e14d83f837791aef880eed3ba2b60f8f43f0
Rita-cedcoss <ritapal@cedcoss.com>
2023-02-03 13:18:02 +0530

    textonomy

--- src/Texonomy_Data/TexonomyData.js
+++ src/Texonomy_Data/TexonomyData.js
@@ -1,97 +1,87 @@
 import React, { useEffect, useState } from 'react'
+import { useRef } from 'react';
 import Data from "./Data.txt"
+let obj={}
+let arr=[];
 const TexonomyData = () => {
-    const[TexonomyData,setTexonomyData]=useState([]);
-    const[MainCat,setMaincat]=useState([]);
     const[nestedArr,setNestedArr]=useState([]);
-    let obj={}
-    let arr=[];
+    const selectRef=useRef([]);
     useEffect(()=>{
         fetch(Data)
         .then(function(response){
             return response.text();
         }).then(function (data) {
             let splitData=data.split("\n");
-        setTexonomyData(splitData)
+        displayData(splitData);
     })
     },[])
-    const displayData=()=>{
-        // let arr=[];
+    const displayData=(TexonomyData)=>{
         TexonomyData.map(item=>{
-            let arr=item.split(">");
+            let arr=item.split(">").map(el=>el.trim());
             createNestedObject(obj,arr);
-            
-            // if(!item.includes(">"))
-            // {
-            //   arr.push({[item]:{}});
-            // }
         })
-        // setNestedArr()
         console.log(obj);
         setNestedArr([obj]);
-        setMaincat([...arr]);
     }
     var createNestedObject = function( base, values ) {
         for( var i = 0; i < values.length; i++ ) {
             base = base[ values[i] ] = base[ values[i] ] || {};
         }
     };
-    console.log(nestedArr)
-    console.log(obj);
-    // const select_category=(e)=>{
-    //     console.log(MainCat[0])
-    //     let cat1=[]
-    //     TexonomyData.map(item=>{   
-    //         if(item.slice(0,item.indexOf('>')).match(e.target.value))
-    //         {                 
-    //             let index=item.slice(item.indexOf('>')+2)
-    //             let catItem=index.slice(0,index.indexOf(">"));
-    //             if(!cat1.includes(catItem)){
-    //                cat1.push(catItem);
-    //             }
-    //         } 
-        //  let cat2=index.slice(0,index.indexOf('>'))
-        //  if(!arr.includes(cat2)){
-        //     arr.push(cat2);
-        //  }
-        //  else{
-
-        //  }
-        // })
-        // let cat2=[]
-        // MainCat.map(item=>{
-        //     // console.log(Object.keys(item).toString())
-       
-        //     if(Object.keys(item).toString()==e.target.value)
-        //     {
-        //         console.log(Object.keys(item).toString());
-        //         cat1.map(ele=>{
-        //             console.log(ele);
-        //             if(!cat2.includes(ele))
-        //             {
-        //                 cat2.push({[ele]:[]})
-        //             }
-        //             // item.push({[ele]:[]})
-        //             // Object.assign(Object.keys(item).toString(),)
-        //         })
-        //        console.log(item,"",cat2 );
-        //     }
-        // })
-        // console.log();
-    
-    // }
+    // console.log(nestedArr);
+    const selectHandler1=(e)=>{
+        select2=[];
+        Object.keys(nestedArr[0]).map((item,i)=>{
+            if(item==e.target.value){
+            let child=Object.values(nestedArr[0])[i];
+            // console.log(child);
+            Object.keys(child).map(ele=>{
+                // console.log(ele);
+             select2.push(ele);
+            })
+            }
+        })  
+        setSelect2([...select2])
+    }
+    const selectHandler2=(e)=>{
+       nestedArr[0].map(item=>{
+        console.log(item);
+       })
+    }
+    const selectHandler=(e)=>{
+        console.log(e.target.value);    
+        console.log(selectRef.current);
+    }
     return (
     <div>
-        <button onClick={displayData}>Texonomy Data</button>
-        <select>
-        {MainCat.map(item=>{
-            // console.log(Object.keys(item).toString())
-        return(<>
-        <option>{Object.keys(item).toString()}</option>
+        {/* <button onClick={displayData}>Texonomy Data</button> */}
+      {nestedArr.length>0?
+      nestedArr.map((item,i)=>{
+        return(
+        <select onChange={()=>selectHandler(i)}  ref={(ref)=>selectRef.current[i]=ref}>
+            {
+                 Object.keys(item).map(ele=>{
+                   return <option value={ele}>{ele}</option>
+                })
+            }
+        </select>)
        
-        </>)
-    })}
-        </select>
+    })
+      :""} 
+        {/* <select onChange={selectHandler1}>
+          {nestedArr.length>0?
+          Object.keys(nestedArr[0]).map(item=>{
+            return<option>{item}</option>
+          })
+          :""}
+        </select> */}
+        {/* <select onChange={selectHandler2}>
+        {select2.map(item=>{
+           return <option>{item}</option>
+        })}
+        </select> */}
+       
+        
     
     </div>
   )
